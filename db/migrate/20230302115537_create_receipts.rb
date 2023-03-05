9e0ca8b4560550b252796da2ffd23f33046c11b4
Oki <okoczek@gmail.com>
2023-03-05 10:44:31 +0200

    Added devise

--- db/migrate/20230302115537_create_receipts.rb
+++ db/migrate/20230302115537_create_receipts.rb
@@ -2,7 +2,7 @@ class CreateReceipts < ActiveRecord::Migration[7.0]
   def change
     create_table :receipts do |t|
       t.date :date, null: false
-      t.integer :shop_id, null: false
+      t.references :shop, null: false, foreign_key: { to_table: :shops }
       t.string :comment, null: true
 
       t.timestamps
