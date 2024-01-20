707e23addd5727efa51d9768325371015de520cd
Oki <okoczek@gmail.com>
2024-01-20 13:55:28 +0200

    Added gems for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  # Any custom configurations here
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
